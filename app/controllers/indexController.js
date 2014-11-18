e205f614ebf6f4cba1f2d99a5f313208783ed2ef
vinothinit <t.vinothini22@gmail.com>
2014-11-18 20:22:49 +0530

    add current timezone and design issues fix

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -7,6 +7,14 @@
 
             var index = this;
             index.date =  new Date();
+
+        var now = new Date().toString();
+        var TZ = now.indexOf('(') > -1 ?
+            now.match(/\([^\)]+\)/)[0].match(/[A-Z]/g).join('') :
+            now.match(/[A-Z]{3,4}/)[0];
+        if (TZ == "GMT" && /(GMT\W*\d{4})/.test(now)) TZ = RegExp.$1;
+
+        index.timezone = TZ;
     }
 
 })();
